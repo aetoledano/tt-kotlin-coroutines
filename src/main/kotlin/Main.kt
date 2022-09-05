4ccfd070436cb6cfaac6ac49ab1e2ffa3f86bc24
Arlen Enrique Toledano Tamayo <arlen.toledano@creditas.com>
2022-09-05 13:46:16 -0500

    Could we do the same with jvm threads ?

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,13 +2,14 @@ import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlin.concurrent.thread
 
-fun main(args: Array<String>) = runBlocking {
-    repeat(1_000_000) { // launch a lot of coroutines
-        launch {
-            print(".")
-        }
+fun main(args: Array<String>) {
+    repeat(1_000_000) { // launch a lot of Threads 
+        Thread {
+            Thread.sleep(3000L)
+        }.start()
     }
     
-    return@runBlocking
+    return
 }
