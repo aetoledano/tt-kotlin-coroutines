2683557938245632425b15aa679aebf9d9cd9c9d
Arlen Enrique Toledano Tamayo <arlen.toledano@creditas.com>
2022-09-05 12:58:46 -0500

    Docs: Some insights about building blocks !

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,11 +3,10 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
-fun main(args: Array<String>) = runBlocking {
+fun main(args: Array<String>) = runBlocking {// coroutine builder
     for (i in 1..10)
-        launch(Dispatchers.Default) {
-            println("I'm -$i- in thread ${Thread.currentThread().name}")
-            delay(1000L)
+        launch { // coroutine builder inherits context
+            delay(1000L) // non-blocking suspending function
             println("I'm -$i- in thread ${Thread.currentThread().name}")
         }
 }
