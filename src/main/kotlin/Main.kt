b36cf0da0bd6cd318ccd8ae821eb5bba7eb0b2d4
Arlen Enrique Toledano Tamayo <arlen.toledano@creditas.com>
2022-09-05 14:11:02 -0500

    Single exception kill them all

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,44 @@
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import kotlin.concurrent.thread
+import java.lang.Exception
 
-fun main(args: Array<String>) {
-    repeat(1_000_000) { // launch a lot of Threads 
-        Thread {
-            Thread.sleep(3000L)
-        }.start()
+fun main(args: Array<String>) = runBlocking {
+    launch {
+        repeat(20) {
+            println(it)
+            delay(1000L)
+        }
+    }
+    launch{
+        good("Mike", 3000L)
+    }
+    launch {
+        good("Iye", 2000L)
+    }
+    launch {
+        good("Itza", 8000L)
+    }
+    launch{
+        bad(5000)
+    }
+    launch {
+        good("He never got a chance",6000)
     }
     
-    return
+    return@runBlocking
+}
+
+suspend fun good(name: String, time: Long) {
+    println("$name has started")
+    delay(time)
+    println("$name has completed")
+}
+
+suspend fun bad(time: Long) {
+    println("BOMB RUNNING")
+    delay(time)
+    throw Exception("exploding ...")
 }
