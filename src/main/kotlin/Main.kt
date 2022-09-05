12617bd3cda6d230eee7ec27a95accd5664f8c6f
Arlen Enrique Toledano Tamayo <arlen.toledano@creditas.com>
2022-09-05 12:32:12 -0500

    All job is completed after last coroutine is over !

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,10 +3,11 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main(args: Array<String>) = runBlocking {
-    launch {
-        delay(1000L)
-        println("world!!")
-    }
+    for (i in 1..10)
+        launch {
+            delay(i*200L)
+            println("world!! $i")
+        }
     
     println("Hello ")
 }
