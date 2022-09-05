88503ac36e71d3397590bf6e3fd8ffac2e1d1be7
Arlen Enrique Toledano Tamayo <arlen.toledano@creditas.com>
2022-09-05 13:10:32 -0500

    Code can be refactored into suspending functions !

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,15 @@
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
-fun main(args: Array<String>) = runBlocking {// coroutine builder
-    for (i in 1..10)
-        launch { // coroutine builder inherits context
-            delay(1000L) // non-blocking suspending function
-            println("I'm -$i- in thread ${Thread.currentThread().name}")
-        }
+fun main(args: Array<String>) = runBlocking {
+    launch {
+        yourFirstSuspendingFunction()
+    }
+    println("hello ")
+    
+    return@runBlocking
+}
+
+suspend fun yourFirstSuspendingFunction() {
+    println("from suspending function !")
 }
